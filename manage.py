48b0ddc9c17c6400135a1f94439339e151740e57
QSS Analytics <141818161+QssAnalytic@users.noreply.github.com>
2025-02-20 16:34:53 +0400

    Update manage.py
    fix

--- manage.py
+++ manage.py
@@ -1,19 +1,30 @@
 import os
 import sys
+import django
+from django.core.management import call_command
+from django.contrib.auth.models import User
 
+# Ayarları başlatmak için
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dsa.settings')
+django.setup()
+
+def create_superuser():
+    username = os.getenv('SUPERUSER_USERNAME', 'admin')
+    email = os.getenv('SUPERUSER_EMAIL', 'admin@example.com')
+    password = os.getenv('SUPERUSER_PASSWORD', 'adminpassword')
+
+    # User modelini kontrol et ve superuser oluştur
+    if not User.objects.filter(username=username).exists():
+        call_command('createsuperuser', interactive=False, username=username, email=email, password=password)
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dsa.settings')
     try:
+        create_superuser()  # Superuser'ı oluştur
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
+        execute_from_command_line(sys.argv)
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
