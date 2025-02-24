19fe557d608cf182716dcc4b1b42418e4b9b03c7
QSS Analytics <141818161+QssAnalytic@users.noreply.github.com>
2025-02-24 15:25:55 +0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,29 +1,13 @@
 import os
 import sys
 import django
-from django.core.management import call_command
-
 
 # Ayarları başlatmak için
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dsa.settings')
 django.setup()
 
-
-from django.contrib.auth.models import User
-def create_superuser():
-    username = os.getenv('SUPERUSER_USERNAME', 'admin')
-    email = os.getenv('SUPERUSER_EMAIL', 'admin@example.com')
-    password = os.getenv('SUPERUSER_PASSWORD', 'adminpassword')
-
-    # User modelini kontrol et ve superuser oluştur
-    user, created = User.objects.get_or_create(username=username)
-    user.password = password
-    user.email = email
-    user.is_staff = True
-    user.is_superuser = True
-    user.save()
 def main():
-    try: # Superuser'ı oluştur
+    try:
         from django.core.management import execute_from_command_line
         execute_from_command_line(sys.argv)
     except Exception as e:
@@ -32,4 +16,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    create_superuser() 
