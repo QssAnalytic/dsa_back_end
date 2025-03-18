0e37c0f7376feec76c3b89190add85ebff4eb18f
QSS Analytics <141818161+QssAnalytic@users.noreply.github.com>
2025-03-18 16:43:23 +0400

    Update settings.py

--- dsa/settings.py
+++ dsa/settings.py
@@ -143,7 +143,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
