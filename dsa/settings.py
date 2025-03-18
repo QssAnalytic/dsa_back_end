ae66c598382b02da7712635a78d0f35f176c48dc
QSS Analytics <141818161+QssAnalytic@users.noreply.github.com>
2025-03-18 13:31:53 +0400

    Update settings.py

--- dsa/settings.py
+++ dsa/settings.py
@@ -12,7 +12,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+}
 
 
 # Superuser bilgilerini ortam değişkenlerinden alıyoruz.
@@ -90,14 +95,7 @@ WSGI_APPLICATION = 'dsa.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
