8f52392106865ea642573a668463b57e7b8e9567
QSS Analytics <141818161+QssAnalytic@users.noreply.github.com>
2025-03-19 20:44:23 +0400

    Update settings.py

--- dsa/settings.py
+++ dsa/settings.py
@@ -150,6 +150,7 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'dsa-media-img'
 AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_DEFAULT_ACL = None
 
 # Medya dosyaları için S3'ü varsayılan depolama yapın
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
