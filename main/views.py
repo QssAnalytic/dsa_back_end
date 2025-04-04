f295d794cc1ccf7f3512fe2257e67424a422ddfb
QSS Analytics <141818161+QssAnalytic@users.noreply.github.com>
2025-04-04 17:27:10 +0400

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,6 +1,14 @@
-from .models import Müraciət, Əlaqə, Qeydiyyat, Bootcamps, BootcampTipi, Təlimlər, Mətinlər, Sessiyalar, Nümayişlər, Sillabuslar, Təlimçilər, Müəllimlər, Məzunlar,FAQ
-from .serializers import MüraciətSerializer, ƏlaqəSerializer, QeydiyyatSerializer, BootcampsSerializer, BootcampTipiSerializer, TəlimlərSerializer, MətinlərSerializer, SessiyalarSerializer, NümayişlərSerializer, SillabuslarSerializer, TəlimçilərSerializer, MüəllimlərSerializer, MəzunlarSerializer,FAQSerializer
+from .models import Müraciət, Əlaqə, Qeydiyyat, Bootcamps, BootcampTipi, Təlimlər, Mətinlər, Sessiyalar, Nümayişlər, Sillabuslar, Təlimçilər, Müəllimlər, Məzunlar, FAQ
+from .serializers import MüraciətSerializer, ƏlaqəSerializer, QeydiyyatSerializer, BootcampsSerializer, BootcampTipiSerializer, TəlimlərSerializer, MətinlərSerializer, SessiyalarSerializer, NümayişlərSerializer, SillabuslarSerializer, TəlimçilərSerializer, MüəllimlərSerializer, MəzunlarSerializer, FAQSerializer
 from rest_framework import viewsets
+from rest_framework.response import Response
+from storages.backends.s3boto3 import S3Boto3Storage
+
+def clean_filename(filename):
+    import os
+    from django.utils.text import slugify
+    name, ext = os.path.splitext(filename)
+    return f"{slugify(name)}{ext}"
 
 class MüraciətViewSet(viewsets.ModelViewSet):
     queryset = Müraciət.objects.all()
@@ -27,8 +35,29 @@ class TəlimlərViewSet(viewsets.ModelViewSet):
     serializer_class = TəlimlərSerializer
 
 class MətinlərViewSet(viewsets.ModelViewSet):
-    queryset = Mətinlər.objects.all().select_related('nümayislər').prefetch_related('trainers', 'syllabus','sessiyalar')
+    queryset = Mətinlər.objects.all().select_related('nümayislər').prefetch_related('trainers', 'syllabus', 'sessiyalar')
     serializer_class = MətinlərSerializer
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        storage = S3Boto3Storage()
+        if 'image' in request.FILES:
+            image_file = request.FILES.getlist('image')[0]
+            file_name = clean_filename(image_file.name)
+            saved_path = storage.save(f"metinler/{file_name}", image_file)
+            instance.image = saved_path
+
+        if 'certificate_image' in request.FILES:
+            cert_file = request.FILES.getlist('certificate_image')[0]
+            file_name = clean_filename(cert_file.name)
+            saved_path = storage.save(f"certificates/{file_name}", cert_file)
+            instance.certificate_image = saved_path
+
+        serializer.save()
+        return Response(serializer.data)
 
 class SessiyalarViewSet(viewsets.ModelViewSet):
     queryset = Sessiyalar.objects.all()
@@ -46,16 +75,59 @@ class TəlimçilərViewSet(viewsets.ModelViewSet):
     queryset = Təlimçilər.objects.all()
     serializer_class = TəlimçilərSerializer
 
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        if 'image' in request.FILES:
+            image_file = request.FILES.getlist('image')[0]
+            storage = S3Boto3Storage()
+            file_name = clean_filename(image_file.name)
+            saved_path = storage.save(f"trainers/{file_name}", image_file)
+            instance.image = saved_path
+
+        serializer.save()
+        return Response(serializer.data)
+
 class MüəllimlərViewSet(viewsets.ModelViewSet):
     queryset = Müəllimlər.objects.all()
     serializer_class = MüəllimlərSerializer
 
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        if 'image' in request.FILES:
+            image_file = request.FILES.getlist('image')[0]
+            storage = S3Boto3Storage()
+            file_name = clean_filename(image_file.name)
+            saved_path = storage.save(f"trainers/{file_name}", image_file)
+            instance.image = saved_path
+
+        serializer.save()
+        return Response(serializer.data)
+
 class MəzunlarViewSet(viewsets.ModelViewSet):
     queryset = Məzunlar.objects.all()
     serializer_class = MəzunlarSerializer
-    
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        if 'image' in request.FILES:
+            image_file = request.FILES.getlist('image')[0]
+            storage = S3Boto3Storage()
+            file_name = clean_filename(image_file.name)
+            saved_path = storage.save(f"graduates/{file_name}", image_file)
+            instance.image = saved_path
+
+        serializer.save()
+        return Response(serializer.data)
+
 class FAQViewSet(viewsets.ModelViewSet):
     queryset = FAQ.objects.all()
     serializer_class = FAQSerializer
-    
-    
